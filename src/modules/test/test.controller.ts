6c76225d72f4c58ca99389a4dbdbab7dcbefcbff
eugene.zenkov <eugene.zenkou@gmail.com>
2022-01-19 17:38:59 +0300

    Add book dto endpoint

--- src/modules/test/test.controller.ts
+++ src/modules/test/test.controller.ts
@@ -1,7 +1,9 @@
-import { Get, Controller } from '@nestjs/common'
-import { ApiBadRequestResponse, ApiOperation, ApiResponse } from '@nestjs/swagger'
+import {Get, Controller, Post, UseFilters, Body, Res, HttpStatus} from '@nestjs/common'
+import {ApiBadRequestResponse, ApiBody, ApiOperation, ApiResponse} from '@nestjs/swagger'
 import {TestService} from "./test.service";
 import {TestEntity} from "../../entity/test-entity";
+import {BookingDto} from "../booking/dto/booking.dto";
+import {DatabaseExceptionFilter, WrongDatesExceptionFilter} from "../../common";
 
 @Controller('test')
 export class TestController {
@@ -14,5 +16,18 @@ export class TestController {
     @Get('/all')
     async getAll (): Promise<TestEntity[]> {
         return await this.testService.getAll()
+    }
+
+    @ApiOperation({ summary: 'Booking auto' })
+    @ApiResponse({ status: 201, description: 'True response'})
+    @ApiBadRequestResponse({description: 'Something wrong'})
+    @ApiBody({ type: BookingDto })
+    @Post('/book')
+    @UseFilters(new WrongDatesExceptionFilter(), new DatabaseExceptionFilter())
+    async bookCar(@Body() bookingDto: BookingDto, @Res() res) {
+        await this.testService.getAll();
+        res
+            .status(HttpStatus.CREATED)
+            .json('The auto has been successfully booked.');
     }
 }
